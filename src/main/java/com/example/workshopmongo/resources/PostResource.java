6f3cca2d3966d655dc23bac09751926745c7a2c3
Edberto Leonardo <edbleonardo@gmail.com>
2020-04-19 18:44:54 -0300

    Consulta simples com query methods

--- src/main/java/com/example/workshopmongo/resources/PostResource.java
+++ src/main/java/com/example/workshopmongo/resources/PostResource.java
@@ -1,13 +1,17 @@
 package com.example.workshopmongo.resources;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.workshopmongo.domain.Post;
+import com.example.workshopmongo.resources.util.URL;
 import com.example.workshopmongo.services.PostService;
 
 @RestController
@@ -16,7 +20,6 @@ public class PostResource {
 
 	@Autowired
 	private PostService postService;
-	
 
 	
 	@RequestMapping(value = "/{id}", method = RequestMethod.GET)
@@ -25,4 +28,10 @@ public class PostResource {
 		return ResponseEntity.ok().body(post);
 	}
 	
+	@RequestMapping(value = "/titlesearch", method = RequestMethod.GET)
+	public ResponseEntity<List<Post>> findByTitle(@RequestParam(value = "text", defaultValue = "") String text){
+		text = URL.decodeParam(text);
+		List<Post> list = postService.findByTitle(text);
+		return ResponseEntity.ok().body(list);
+	}
 }
--- src/main/java/com/example/workshopmongo/services/PostService.java
+++ src/main/java/com/example/workshopmongo/services/PostService.java
@@ -1,5 +1,6 @@
 package com.example.workshopmongo.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -28,4 +29,10 @@ public class PostService {
 		}
 		return post;
 	}*/
+	
+	public List<Post> findByTitle(String text){
+		return postRepository.findByTitleContainingIgnoreCase(text);
+	}
+	
+	
 }
